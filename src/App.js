d58005277b4735e5e70f6460375a8abc399428d0
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-11-21 18:06:31 +0100

    Menus with icons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Test from './pruebas/Test'
 import FormTest from './pruebas/FormTest'
 import FormTest2 from './pruebas/FormTest2'
 import TableTest from './pruebas/TableTest'
+import LayoutTest from './pruebas/LayaoutTest'
+import MenuVerticalTest from './pruebas/MenuVerticalTest'
 
 export const history = createBrowserHistory()
 
@@ -17,6 +19,8 @@ const App = () => (
         <Route path="/ftest" component={FormTest} exact={true} />
         <Route path="/ftest2" component={FormTest2} exact={true} />
         <Route path="/ttest" component={TableTest} exact={true} />
+        <Route path="/layout" component={LayoutTest} exact={true} />
+        <Route path="/menuv" component={MenuVerticalTest} exact={true} />
         <Route path="/test" component={Test} exact={true} />
         <Route component={NotFound} />
       </Switch>
